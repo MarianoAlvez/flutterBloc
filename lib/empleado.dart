7269cab9f70067f4407e3981ae809319e638fe8b
marianoAlvez <alvez_jmariano@hotmail.com>
2022-06-14 15:29:16 -0300

    initial commit

---
+++ lib/empleado.dart
@@ -0,0 +1,30 @@
+class Empleado {
+  int _id;
+  String _nombre;
+  double _salario;
+
+  //constructor
+  Empleado(
+    this._id,
+    this._nombre,
+    this._salario,
+  );
+
+  //setter
+  set id(int id) {
+    this._id = id;
+  }
+
+  setnombred(String nombre) {
+    this._nombre = nombre;
+  }
+
+  set salario(double salario) {
+    this._salario = salario;
+  }
+
+  //getter
+  int get id => this._id;
+  String get nombre => this._nombre;
+  double get salario => this._salario;
+}
---
+++ lib/empleadoBloc.dart
@@ -0,0 +1,7 @@
+// 2. Lista de epleados
+// 3. Stream Controllers
+// 4. Stream Sink Getters
+// 5. Constructor - añadir data, escuchar cambios
+// 6. dispose
+// 1. imports
+
